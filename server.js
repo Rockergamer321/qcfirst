fba595edbaed3d91040456f61ee24e09590ca7d1
Jonathan Dinh <jonathanmdinh@gmail.com>
2021-05-04 00:53:02 -0400

    attempt at email constraint

--- server.js
+++ server.js
@@ -39,12 +39,21 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
   });
 
   app.post(
-    '/studentsignup', (req, res) => {
-      students.insertOne(req.body)
-      .then(result => {
-      res.redirect("https://qcfirst.herokuapp.com/login.html")
-      })
-      .catch(error => console.error(error))
+    '/studentsignup', [
+      check('email').custom(async function(value) {
+        var user = await students.find({email:value})
+        return user.length == 0;
+      }).withMessage('Email is already in use')
+    ],
+    (req, res) => {
+        var errors = validationResult(req);
+        if(!errors.isEmpty()) {
+          students.insertOne(req.body)
+          .then(result => {
+          res.redirect("https://qcfirst.herokuapp.com/login.html")
+          })
+        .catch(error => console.error(error))
+        }
     })
 
   //When the form on the teacher-signup.html page is submitted, a new entry will be submitted
@@ -62,7 +71,7 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
   app.post('/createaclass', (req, res) => {
     classes.insertOne(req.body)
     .then(result => {
-       res.redirect('/login.html')
+       res.redirect('/')
     })
     .catch(error => console.error(error))
   }) 
