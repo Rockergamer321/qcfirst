dd1e488c3735837b760c128c217520fcedd8f3a2
Jonathan Dinh <jonathanmdinh@gmail.com>
2021-05-19 12:57:56 -0400

    added display of links to hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function openMenu() {
+  var x = document.getElementById("menuNav");
+  if (x.className === "header-right") {
+    x.className += " responsive";
+  } else {
+    x.className = "header-right";
+  }
+}
+
+function opentopnav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+//index.html slide carousel implementation
 var slideIndex = 1;
 showSlides(slideIndex);
 
